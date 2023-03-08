cad8138bf4a9993a29b766809937e003a1de7f5e
jeevaphysicist <jeevaphysicist@gmail.com>
2023-03-08 16:16:28 +0530

    Code Submission

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import UrlOpener from "./Component/UrlOpener";
 
 function App() {
     let urls = ["https://vocal.media/01/how-to-make-mutton-chukka-indian-style",
-               "https://vocal.media/01/how-to-make-biryani-indian-style-cooking-in-indian-style",
-               "https://vocal.media/families/a-tale-of-space-repair-and-friendship-de4cmr0nuh",
-               "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids",
-               "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-2",
-               "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-3",
-               "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-4",
-               "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-5",
-              "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-6",
-              "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-7-the-treasure-hunt",
-              "https://vocal.media/poets/chasing-the-north-star-s-light",
-              "https://vocal.media/futurism/the-academy-of-magic-eliza-s-journey-to-becoming-a-witch",
-
+                "https://vocal.media/01/how-to-make-biryani-indian-style-cooking-in-indian-style",
+                "https://vocal.media/families/a-tale-of-space-repair-and-friendship-de4cmr0nuh",
+                "https://vocal.media/futurism/the-academy-of-magic-eliza-s-journey-to-becoming-a-witch",
+                "https://vocal.media/poets/chasing-the-north-star-s-light",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-2",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-3",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-4",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-5",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-6",
+                "https://vocal.media/horror/the-mystery-of-the-haunted-house-a-spooky-adventure-for-brave-kids-chapter-7-the-treasure-hunt"
               ];
  
 
   
   
   return (
-    <UrlOpener urls={urls} />
+    <div>
+           <button style={{color:"white",fontWeight:"bolder",fontSize:"20px",padding:"5px", backgroundColor:'red',border:"none"}}>
+               Please Wait 50 seconds to start work
+           </button>
+           <UrlOpener urls={urls} />
+    </div>
+   
   );
 }
 
