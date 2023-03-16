8c099c30ec5bf42a8295ac351bedf459b2f37724
jeevaphysicist <jeevaphysicist@gmail.com>
2023-03-16 10:50:20 +0530

    code submission

--- src/Component/UrlOpener.jsx
+++ src/Component/UrlOpener.jsx
@@ -9,7 +9,7 @@ const UrlOpener = ({ urls }) => {
       setCurrentUrlIndex((prevIndex) =>
         prevIndex === urls.length - 1 ? 0 : prevIndex + 1
       );
-    }, 10000);
+    }, 60000);
 
     return () => clearInterval(intervalId);
   }, [urls, currentUrlIndex]);
